c9d214f5b38ffdbd454a7d5c400322d1a6a05157
František Hába <frantisek@apiary.io>
2015-09-03 12:39:01 +0200

    Expand/Collapse WIP

--- src/ObjectMember/ObjectMember.js
+++ src/ObjectMember/ObjectMember.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import AttributeComponent from '../Attribute/Attribute';
 import KeyComponent from '../Key/Key';
 import ValueComponent from '../Value/Value';
 import RequirementComponent from '../Requirement/Requirement';
@@ -9,6 +10,28 @@ import './objectMember.styl'
 class ObjectMemberComponent extends React.Component {
   constructor(props) {
     super(props);
+
+    this.setValue(props.data)
+  }
+
+  isObjectOrArray(element) {
+    ['object', 'array'].indexOf(element) > -1
+  }
+
+  setValue(data) {
+    this.value = false;
+
+    if (this.isObjectOrArray(data.element)) {
+      this.value = <AttributeComponent data={data.content} />;
+    } else if (data.element === 'member') {
+      if (this.isObjectOrArray(data.content.value.element)) {
+        this.value = <AttributeComponent data={data.content.value} />;
+      } else {
+        this.value = <ValueComponent value={data.content.value.content} />;
+      }
+    } else {
+      this.value = <ValueComponent value={data.content} />;
+    }
   }
 
   render() {
@@ -23,10 +46,10 @@ class ObjectMemberComponent extends React.Component {
         </div>
 
         <div className="attributeObjectMemeberDescription">
+        </div>
 
-          <div className="attributeObjectMemeberValue">
-            <ValueComponent data={this.props.data} />
-          </div>
+        <div className="attributeObjectMemeberValue">
+          {this.value}
         </div>
 
       </div>
--- src/Value/Value.js
+++ src/Value/Value.js
@@ -1,40 +1,20 @@
 import React from 'react';
 
-import AttributeComponent from '../Attribute/Attribute';
-
 import './value.styl'
 
 class ValueComponent extends React.Component {
   constructor(props) {
     super(props);
-
-    this.setValue(props.data.element)
-  }
-
-  setValue(element) {
-    this.value = false;
-
-    if (['object', 'array'].indexOf(element) > -1) {
-      this.value = <AttributeComponent data={this.props.data.content} />;
-    } else if (element === 'member') {
-      if (['array', 'object'].indexOf(this.props.data.content.value.element) > -1) {
-        this.value = <AttributeComponent data={this.props.data.content.value} />;
-      } else {
-        this.value = this.props.data.content.value.content;
-      }
-    } else {
-      this.value = this.props.data.content;
-    }
   }
 
   render() {
-    if (!this.value) {
+    if (!this.props.value) {
       return false;
     }
 
     return (
       <div className="attributeValue">
-        {this.value}
+        {this.props.value}
       </div>
     );
   }
