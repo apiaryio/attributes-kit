dc139419df865a9b1b3a8c2858b92622ebad163d
XVincentX <vincenz.chianese@yahoo.it>
2015-09-04 12:43:28 +0200

    Changed Url

--- scripts/visual-testing.js
+++ scripts/visual-testing.js
@@ -3,7 +3,7 @@ import launcher from 'browser-launcher2';
 
 launcher.detect( (available) => {
   launcher((launcherErr, launch) => {
-    launch('http://www.microsoft.com', {
+    launch('http://localhost:9090', {
       detached: true,
       browser: available[0].name,
     }, (launchErr, instance) => {
