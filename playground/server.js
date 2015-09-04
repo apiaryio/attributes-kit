af6115d8f9cff7e5ec5e5ac068f9259011c812f2
XVincentX <vincenz.chianese@yahoo.it>
2015-09-04 15:56:48 +0200

    Separated parseMson function so it can be reused in bulk parsing

--- playground/server.js
+++ playground/server.js
@@ -2,6 +2,7 @@ import Express from 'express';
 import BodyParser from 'body-parser';
 import Protagonist from 'protagonist';
 import Dedent from 'dedent'
+import mson_zoo from 'mson-zoo'
 
 // Starts server
 const app = Express();
@@ -10,16 +11,36 @@ app.use(BodyParser.json());
 app.use(Express.static('dist'));
 app.use('/', Express.static(__dirname + '/views'));
 
-app.post('/parse', function(req, res) {
-  const lines = req.body.source.split('\n');
+app.post('/parse', (req, res) => {
+
+  const attributes = parseMson(req.body.source, (err, attributes) => {
+    if (err) {
+      return res.status(400).json({error: err});
+    } else {
+      return res.json(attributes);
+    }
+  });
+});
+
+app.get('/visual-data', (req, res) => {
+  res.json(mson_zoo.samples);
+})
+
+const server = app.listen(9090, 'localhost', () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Server is listening on ${host}:${port}`);
+});
+
+
+const parseMson = (mson, cb) => {
+  const lines = mson.split('\n');
   let source = Dedent`
     FORMAT: 1A
     # Attributes
-
     # Group Test
-
     ## Test [/test]
-
     + Attributes
     `;
 
@@ -29,13 +50,12 @@ app.post('/parse', function(req, res) {
   });
 
   source += Dedent`
-
     ### Retrieve [GET]
   `;
 
   Protagonist.parse(source.trim(), function(err, result) {
     if (err) {
-      return res.status(400).json({error: err});
+      return cb(err);
     }
 
     let attributes = null;
@@ -48,23 +68,13 @@ app.post('/parse', function(req, res) {
         if (resource.content[0]) {
           const dataStructure = resource.content[0];
           if (dataStructure.content[0]) {
-            attributes = dataStructure.content[0];
+            return cb(err, dataStructure.content[0]);
           }
         }
       }
     }
 
-    if (attributes) {
-      res.json(attributes);
-    } else {
-      res.status(400).json({error: 'No attribute parsed'});
-    }
+    return cb('No attribute parsed');
+
   });
-});
-
-const server = app.listen(9090, 'localhost', () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log(`Server is listening on ${host}:${port}`);
-});
+};
