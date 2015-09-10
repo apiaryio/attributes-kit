2c69ed18c832bc8503b5719c812286ab78a3116c
Ondřej Musil <ondra@apiary.io>
2015-09-10 15:51:20 +0200

    Unified filenames and component names

--- playground/app.js
+++ playground/app.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-import PlaygroundComponent from './components/playground';
-import VisualTestingComponent from './components/visual-testing';
+import Playground from './components/Playground';
+import VisualTesting from './components/VisualTesting';
 
 import './styles/layout.styl';
 
 console.log('Welcome in playground');
 
 if (document.getElementById('playgroundContainer') !== null) {
-  React.render(<PlaygroundComponent />, document.getElementById('playgroundContainer'));
+  React.render(<Playground />, document.getElementById('playgroundContainer'));
 }
 
 if (document.getElementById('visualtestingContainer') !== null) {
-  React.render(<VisualTestingComponent />, document.getElementById('visualtestingContainer'));
+  React.render(<VisualTesting />, document.getElementById('visualtestingContainer'));
 }
-
