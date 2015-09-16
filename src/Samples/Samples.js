87406c649174d889552ac0c4321504873a174aff
Ondřej Musil <ondra@apiary.io>
2015-09-16 18:56:29 +0200

    WIP

--- src/Samples/Samples.js
+++ src/Samples/Samples.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import Sample from 'Sample/Sample';
 
@@ -7,9 +8,28 @@ class Samples extends React.Component {
     data: React.PropTypes.array,
   }
 
+  constructor(props) {
+    super(props);
+
+    // State hasn't been set; tree hidden by default
+    // after a click, it collapses.
+    this.state = {
+      isExpanded: false,
+    };
+  }
+
+  getClassNames() {
+    return classNames({
+        'attributeSamples': true,
+        'isExpanded': this.state.isExpanded,
+        'isCollapsed': !this.state.isExpanded,
+      }
+    );
+  }
+
   render() {
     return (
-      <div className="attributeSamples">
+      <div className={this.getClassNames()}>
         {this.props.data.map((sample, index) => {
           return (
             <Sample key={index} data={sample} />
