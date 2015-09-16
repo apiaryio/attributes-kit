5a9e1388aaecbd72d3fe1ca11500f11e5923dec0
XVincentX <vincenz.chianese@yahoo.it>
2015-09-16 14:02:33 +0200

    Updated paths

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -2,8 +2,8 @@ import WebpackDevServer from 'webpack-dev-server';
 import Webpack from 'webpack';
 import path from 'path';
 
-import playgroundConfig from './playground.config';
-import testConfig from './test.config';
+import playgroundConfig from '../webpack/playground.config';
+import testConfig from '../webpack/test.config';
 import '../playground/server';
 
 // Starts webpack-dev-server
