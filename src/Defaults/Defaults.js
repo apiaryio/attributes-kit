daca079672a57688db82677954b32ed547f5bff2
Ondřej Musil <ondra@apiary.io>
2015-09-17 15:50:20 +0200

    Created defaults component

---
+++ src/Defaults/Defaults.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import SimpleValue from 'SimpleValue/SimpleValue';
+
+class Defaults extends React.Component {
+  static propTypes = {
+    data: React.PropTypes.array
+  }
+
+  renderDefaultEntries() {
+    const data = this.props.data;
+    if (Array.isArray(data)) {
+      return data.map((entry, index) => {
+        return (
+          <SimpleValue key={index} data={entry} />
+        );
+      });
+    }
+
+    return data;
+  }
+
+  render() {
+    return (
+      <div className="attributeDefaults">
+        <div className="attributeDefaultsEntries">
+          {this.renderDefaultEntries()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Defaults;
