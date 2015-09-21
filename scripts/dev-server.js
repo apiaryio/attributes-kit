07bb4c1aeb016e83e2861eaf732c385d47aa030c
XVincentX <vincenz.chianese@yahoo.it>
2015-09-21 12:41:36 +0200

    Unified test and playground

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -2,15 +2,8 @@ import WebpackDevServer from 'webpack-dev-server';
 import Webpack from 'webpack';
 import path from 'path';
 
-import playgroundConfig from '../webpack/playground.config';
-import testConfig from '../webpack/test.config';
+import webpackConfig from '../webpack/playground.config';
 import '../playground/server';
-
-// Starts webpack-dev-server
-var webpackConfig = playgroundConfig;
-if (process.env.NODE_ENV === 'test') {
-  webpackConfig = testConfig;
-}
 
 const compiler = Webpack(webpackConfig);
 const webpackServer = new WebpackDevServer(compiler, {
