4a279a0b7c4302e9ca58e767afcf34b06abb1497
XVincentX <vincenz.chianese@yahoo.it>
2015-09-21 11:21:53 +0200

    Let's use mson zoo directly

--- scripts/generateFixtures.js
+++ scripts/generateFixtures.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import request from 'superagent'
+import mson_zoo from 'mson-zoo'
 
 
 import Attributes from '../dist/server'
-import '../playground/server'
 
-request
-  .get('http://localhost:9090/fixtures')
-  .set('Accept', 'application/json')
-  .end((err, res) => {
-    res.body.forEach((element) => {
-      const renderedElement = React.createElement(Attributes, {
-        data: element
-      });
-
-      const htmlString = React.renderToString(renderedElement);
-      console.log(htmlString);
-    });
-
-    process.exit(0);
-
+mson_zoo.samples.forEach((element) => {
+  const renderedElement = React.createElement(Attributes, {
+    data: element
   });
 
+  const htmlString = React.renderToString(renderedElement);
+  console.log(htmlString);
+});
 
+process.exit(0);
