997fbb771e84e86e12814a9b382909513830ba14
XVincentX <vincenz.chianese@yahoo.it>
2015-09-21 11:23:27 +0200

    Saving sample to file

--- scripts/generateFixtures.js
+++ scripts/generateFixtures.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import request from 'superagent'
 import mson_zoo from 'mson-zoo'
+import fs from 'fs'
 
 
 import Attributes from '../dist/server'
 
-mson_zoo.samples.forEach((element) => {
+mson_zoo.samples.forEach((sample) => {
   const renderedElement = React.createElement(Attributes, {
-    data: element
+    data: sample.code
   });
 
   const htmlString = React.renderToString(renderedElement);
-  console.log(htmlString);
+  fs.writeFileSync(sample.name, htmlString);
 });
 
 process.exit(0);
