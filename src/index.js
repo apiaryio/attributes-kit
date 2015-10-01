d69d239825668f83417275079cd8f25f2245639b
Ondřej Musil <ondra@apiary.io>
2015-10-01 15:47:28 +0200

    Unified interface for server and client builds

--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
+import React from 'react';
+import {EventEmitter} from 'events';
+
 import Attributes from 'Attributes/Attributes';
 import Attribute from 'Attribute/Attribute';
 
-export default {
-  Attributes,
-  Attribute,
-};
+class AttributesKit extends EventEmitter {
+  static render(data, element, options) {
+    let elementSelector = null;
+    if (typeof element === 'string') {
+      elementSelector = element;
+    }
+
+    const attributesKit = new AttributesKit({
+      element,
+      elementSelector,
+      options,
+    });
+
+    attributesKit.render(data);
+    return attributesKit;
+  }
+
+  static Attributes = Attributes;
+  static Attribute = Attribute;
+
+  constructor(options) {
+    super();
+
+    this.options = options || {};
+
+    this.element = options.element;
+    this.elementSelector = options.elementSelector;
+    this.options = options.options;
+
+    this.getElement();
+    this.validate();
+  }
+
+  validate() {
+    if (!this.element) {
+      throw Error('Unable to find element where to render attributes.');
+    }
+  }
+
+  getElement() {
+    if (this.element) {
+      return this.element;
+    }
+
+    this.element = document.querySelector(this.elementSelector);
+    return this.element;
+  }
+
+  render(data) {
+    React.render(<Attributes data={data} />, this.element);
+  }
+}
+
+// Exports
+export default AttributesKit;
