78d563cfd4b7f581160acb8883f593f706c2e0c0
XVincentX <vincenz.chianese@yahoo.it>
2015-10-01 13:46:28 +0200

    Use the isProduction flag

--- webpack/playground.config.js
+++ webpack/playground.config.js
@@ -3,21 +3,34 @@ import path from 'path';
 import webpack from 'webpack';
 import baseConfig from './base.config';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default _.extend({}, baseConfig, {
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080',
-    'webpack/hot/only-dev-server',
-    './playground/app'
-  ],
+  entry: (function() {
+    let array = [
+      './playground/app'
+    ];
+
+    if (!isProduction) {
+      array.unshift('webpack/hot/only-dev-server');
+      array.unshift('webpack-dev-server/client?http://0.0.0.0:8080');
+    }
+
+    return array;
+
+  })(),
 
   output: {
     path: path.join(__dirname, '../dist'),
     filename: 'playground.js'
   },
 
-  plugins: (function(){
-    if (process.env.NODE_ENV !== 'production')
-      return [new webpack.HotModuleReplacementPlugin()]
+  plugins: (function() {
+    if (!isProduction) {
+      return [new webpack.HotModuleReplacementPlugin()];
+    }
+
+    return [];
   })(),
 
   devtool: 'source-map'
