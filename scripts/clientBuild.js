ee458f3d49f55b716ee2290f7a0d3609ce2b9f50
XVincentX <vincenz.chianese@yahoo.it>
2015-10-05 13:07:01 +0200

    Provide new build for browser

--- scripts/clientBuild.js
+++ scripts/clientBuild.js
@@ -2,6 +2,35 @@ import Webpack from 'webpack';
 import path from 'path';
 
 import clientConfig from '../webpack/client.config';
+if (process.argv.length > 3 ) {
+  if (process.argv[3] == 'full') {
+    console.info("Performing a full dep client build.")
+    clientConfig.externals = {};
+  }
+  else if (process.argv[3] == 'none') {
+    console.info("Performing a no dep client build.")
+   clientConfig.externals = {
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'classnames': {
+      root: 'classnames',
+      commonjs2: 'classnames',
+      commonjs: 'classnames',
+      amd: 'classnames'
+    },
+    'marked': {
+      root: 'marked',
+      commonjs2: 'marked',
+      commonjs: 'marked',
+      amd: 'marked'
+    },
+   };
+  }
+}
 
 const compiler = Webpack(clientConfig, (err, stat) => {
   console.info('Webpack client build done.');
