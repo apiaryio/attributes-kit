1fcf675ce7415c6b12436ac0dea3c21b521b8674
XVincentX <vincenz.chianese@yahoo.it>
2015-11-02 17:15:52 +0100

    Added react dom to excluded deps for noReact build

--- webpack/client.config.js
+++ webpack/client.config.js
@@ -37,6 +37,13 @@ export default _.extend({}, baseConfig, {
       commonjs2: 'react',
       commonjs: 'react',
       amd: 'react'
+    },
+    'react-dom': {
+      root: 'react-dom',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
     }
+
   }
 });
