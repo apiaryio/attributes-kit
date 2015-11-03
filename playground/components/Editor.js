af604f7646b162e8bc2fbf3716d00bbd805a8569
XVincentX <vincenz.chianese@yahoo.it>
2015-11-03 15:00:50 +0100

    Do not lint vendor file

--- playground/components/Editor.js
+++ playground/components/Editor.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import EditorActions from '../actions/editor';
 
-import TabOverride from './taboverride';
+import TabOverride from './TabOverride';
 
 class EditorComponent extends React.Component {
-
-  _handleChange = (event) => {
-    this.setState({value: event.target.value});
-    EditorActions.parse(event.target.value);
-  }
 
   componentDidMount() {
     TabOverride.tabSize(4);
     TabOverride.autoIndent(true);
     TabOverride.set(this.refs.textarea);
+  }
+
+  _handleChange = (event) => {
+    this.setState({value: event.target.value});
+    EditorActions.parse(event.target.value);
   }
 
   render() {
