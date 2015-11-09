3ba76baaffa7a6ac0aeeb0caf3d814b6a558c385
František Hába <frantisek@apiary.io>
2015-11-09 15:18:32 +0100

    Redesign the Value component, use inline styles.

--- src/Value/Value.js
+++ src/Value/Value.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import _ from 'lodash';
 
-import './value.styl';
-
+import {
+  isObjectOrArray,
+  isMember,
+} from 'elements/element';
 
 class Value extends React.Component {
   static propTypes = {
@@ -10,14 +13,69 @@ class Value extends React.Component {
       React.PropTypes.number,
       React.PropTypes.bool,
     ]),
-  };
+    element: React.PropTypes.object,
+  }
+
+  renderStyles() {
+    let styles = {
+      float: 'left',
+      width: 'auto',
+      height: 'auto',
+      margin: '0',
+      padding: '0',
+      fontWeight: '600',
+      backgroundColor: '#f0f1f4',
+      borderRadius: '4px',
+      paddingTop: '2px',
+      paddingBottom: '2px',
+      paddingLeft: '4px',
+      paddingRight: '4px',
+      fontFamily: 'Source Sans Pro',
+      fontSize: '15px',
+      color: '#4C5264',
+      lineHeight: '19px',
+    };
+
+    return _.merge(styles, this.props.style || {});
+  }
 
   render() {
-    return (
-      <div className="attributeValue">
-        {this.props.value}
-      </div>
-    );
+    if (this.props.value) {
+      return (
+        <div style={this.renderStyles()}>
+          {this.props.value}
+        </div>
+      );
+    }
+
+    if (this.props.element) {
+      const element = this.props.element;
+      let value;
+
+      if (isObjectOrArray(element.element)) {
+        return false;
+      } else if (isMember(element.element)) {
+        if (isObjectOrArray(element.content.value.element)) {
+          return false;
+        } else if (element.content.value.content) {
+          value = element.content.value.content;
+        } else {
+          return false;
+        }
+      } else if (element.content) {
+        value = element.content;
+      }
+
+      if (!value) {
+        return false;
+      }
+
+      return (
+        <div style={this.renderStyles()}>
+          {value}
+        </div>
+      );
+    }
   }
 }
 
