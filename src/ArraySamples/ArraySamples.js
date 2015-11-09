2bfd4f83d966041a38fd731e881e05aa2353dcf4
František Hába <frantisek@apiary.io>
2015-11-09 15:25:38 +0100

    Support array samples.

---
+++ src/ArraySamples/ArraySamples.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import ArraySample from 'ArraySample/ArraySample'
+
+
+class ArraySamples extends React.Component {
+  static propTypes = {
+
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  renderStyles() {
+    let styles = {
+      root: {}
+    };
+
+    return styles;
+  }
+
+  render() {
+    const styles = this.renderStyles();
+
+    if (!this.props.element) {
+      return false;
+    }
+
+    const attributes = this.props.element.attributes;
+
+    if (!attributes) {
+      return false;
+    }
+
+    const samples = attributes.samples;
+
+    if (!samples) {
+      return false;
+    }
+
+    return (
+      <div style={styles.root}>
+        {
+          samples.map((sample, index) => {
+            return (
+              <ArraySample
+                element={this.props.element}
+                samples={sample}
+                key={index}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default ArraySamples;
