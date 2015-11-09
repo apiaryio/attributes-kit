36f976b177d425e2a4a22870e3a1ea1e2c87a49a
František Hába <frantisek@apiary.io>
2015-11-09 15:21:32 +0100

    Add a new component to display header of an array.

---
+++ src/ArrayHeader/ArrayHeader.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import Type from 'Type/Type';
+import SampleToggle from 'SampleToggle/SampleToggle';
+
+import {
+  hasSamples
+} from 'elements/element';
+
+
+class ArrayHeader extends React.Component {
+  static propTypes = {
+
+  }
+
+  renderStyles() {
+    let styles = {
+      root: {
+        border: '1px solid #E8EBEE',
+        borderBottom: '1px solid #E8EBEE',
+        height: 'auto',
+        paddingBottom: '6px',
+        paddingLeft: '8px',
+        paddingTop: '4px',
+        width: '100%',
+        backgroundColor: '#F8F8F9',
+      },
+      sampleToggle: {
+        root: {
+          marginTop: '4px',
+        }
+      },
+      type: {
+        root: {
+          fontSize: '12px',
+        }
+      }
+    };
+
+    if (this.props.isExpanded) {
+      styles.root.borderBottom = 'none';
+    }
+
+    return styles;
+  };
+
+  renderSampleToggle(styles) {
+    if (this.props.element && hasSamples(this.props.element)) {
+      return (
+        <SampleToggle
+          onClick={this.props.onSampleToggleClick}
+          style={styles.sampleToggle}
+          isExpanded={this.props.isExpanded}
+          sampleTitle={this.props.sampleTitle}
+        />
+      );
+    }
+  }
+
+  render() {
+    const styles = this.renderStyles();
+
+    return (
+      <div style={styles.root}>
+        <Type
+          type="array"
+          style={styles.type} />
+
+        {this.renderSampleToggle(styles)}
+      </div>
+    );
+  }
+}
+
+export default ArrayHeader;
