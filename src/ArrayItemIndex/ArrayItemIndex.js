ba3f42cd3038e2175e3e18a71cc015a2b608c72d
František Hába <frantisek@apiary.io>
2015-11-09 15:24:21 +0100

    Add a new component to display index of an array item.

---
+++ src/ArrayItemIndex/ArrayItemIndex.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+class ArrayItemIndex extends React.Component {
+  static propTypes = {
+    index: React.PropTypes.number,
+  }
+
+  renderStyles() {
+    let styles = {
+      root: {
+        position: 'absolute',
+        top: '8px',
+        right: '8px',
+        fontFamily: 'Source Code Pro',
+        fontSize: '14px',
+        color: '#C8CDD4',
+        lineHeight: '18px',
+        color: '#C8CDD4'
+      }
+    };
+
+    return styles;
+  }
+
+  render() {
+    const styles = this.renderStyles();
+
+    return (
+      <span style={styles.root}>
+        {this.props.index}
+      </span>
+    );
+  }
+}
+
+export default ArrayItemIndex;
