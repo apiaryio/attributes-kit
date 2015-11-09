a03a82bea304a6ad1e364f8a72a021991dadcfd8
František Hába <frantisek@apiary.io>
2015-11-09 15:52:56 +0100

    Support samples of an object property.

---
+++ src/ObjectPropertySamples/ObjectPropertySamples.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import Sample from 'Sample/Sample';
+
+import Row from 'Row/Row';
+import Column from 'Column/Column';
+
+import {
+  isObjectOrArray,
+} from 'elements/element';
+
+
+class ObjectPropertySamples extends React.Component {
+  static propTypes = {
+    element: React.PropTypes.object,
+  }
+
+  renderStyles() {
+    return {
+      row: {
+        marginTop: '2px',
+      }
+    };
+  }
+
+  renderSamples(samples) {
+    let style = {
+      row: {
+        marginTop: '6px',
+      }
+    };
+
+    return samples.map((sample, index) => {
+      return (
+        <Sample sample={sample} key={index} style={style} />
+      );
+    });
+  }
+
+  render() {
+    if (!this.props.element) {
+      return false;
+    }
+
+    if (!this.props.element.content) {
+      return false;
+    }
+
+    const value = this.props.element.content.value;
+
+    if (!value) {
+      return false;
+    }
+
+    if (isObjectOrArray(value.element)) {
+      return false;
+    }
+
+    let samples = null;
+
+    if (value.attributes) {
+      samples = value.attributes.samples;
+    }
+
+    if (!samples) {
+      return false;
+    }
+
+    const styles = this.renderStyles();
+
+    return (
+      <Row style={styles.row}>
+        <Column>
+          {this.renderSamples(samples)}
+        </Column>
+      </Row>
+    );
+  }
+}
+
+export default ObjectPropertySamples;
