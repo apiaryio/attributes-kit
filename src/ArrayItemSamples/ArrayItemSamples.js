f63705e502bda0018d7f23e78c130041f45feb7d
František Hába <frantisek@apiary.io>
2015-11-09 15:25:48 +0100

    Support array item samples.

---
+++ src/ArrayItemSamples/ArrayItemSamples.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import Sample from 'Sample/Sample';
+
+import Row from 'Row/Row';
+import Column from 'Column/Column';
+
+import {
+  isObjectOrArray,
+} from 'elements/element';
+
+
+class ArrayItemSamples extends React.Component {
+  static propTypes = {
+    element: React.PropTypes.object,
+  }
+
+  renderStyles() {
+    return {
+      row: {
+        marginTop: '2px',
+      }
+    };
+  }
+
+  renderSamples(samples) {
+    let style = {
+      row: {
+        marginTop: '6px',
+      }
+    };
+
+    return samples.map((sample, index) => {
+      return (
+        <Sample sample={sample} key={index} style={style} />
+      );
+    });
+  }
+
+  render() {
+    if (!this.props.element) {
+      return false;
+    }
+
+    if (!this.props.element.attributes) {
+      return false;
+    }
+
+    const samples = this.props.element.attributes.samples;
+
+    if (!samples) {
+      return false;
+    }
+
+    const styles = this.renderStyles();
+
+    return (
+      <Row style={styles.row}>
+        <Column>
+          {this.renderSamples(samples)}
+        </Column>
+      </Row>
+    );
+  }
+}
+
+export default ArrayItemSamples;
