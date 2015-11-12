3ae5fdbd8251427697ca3aec5d577131fe5ca06a
XVincentX <vincenz.chianese@yahoo.it>
2015-11-12 10:51:26 +0100

    Wrapping all the errors

---
+++ src/Error/Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class Error extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <label>I am sorry, I was not able to render this thing</label>
+      </div>
+    );
+  }
+}
+
+export default Error;
