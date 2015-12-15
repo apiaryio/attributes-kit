6454545be6357151562ca00275afbcf47259d192
XVincentX <vincenz.chianese@yahoo.it>
2015-12-15 12:13:51 +0100

    Added default Layout component

---
+++ playground/components/DefaultLayout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class DefaultLayout extends React.Component {
+  static propTypes = {
+    children: React.propTypes.array,
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header">
+        <div className="leftSide"><span>Attributes Kit Playground</span></div>
+        <div className="rightSide">
+          <span>
+            <a target="_blank" className="gitHubIco" href="https://github.com/apiaryio/attributes-kit">Attributes Kit Repository</a>
+          </span>
+          <span>
+            <a target="_blank" className="documentationIco" href="https://github.com/apiaryio/mson">Mson Documentation</a>
+          </span>
+        </div>
+        </div>
+        <div className="body">
+          {this.props.children}
+      </div>
+    </div>
+    );
+  }
+}
+
+export default DefaultLayout;
