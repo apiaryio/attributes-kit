43b64eef5ce4a3325a11f330bf33fa5d582d259a
František Hába <frantisek@apiary.io>
2016-02-11 15:18:12 +0100

    Playground: Dereference by default

--- playground/app.js
+++ playground/app.js
@@ -5,7 +5,8 @@ import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
 
 import Playground from './components/Playground';
-import VisualTesting from './components/VisualTesting';
+import Examples from './components/Examples';
+
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 import './styles/layout.styl';
@@ -14,6 +15,6 @@ render((
   <Router history={createBrowserHistory()}>
     <Route path="/" component={Playground}/>
     <Route path="/playground" component={Playground}/>
-    <Route path="/examples" component={VisualTesting}/>
+    <Route path="/examples" component={Examples}/>
   </Router>
 ), document.getElementById('applicationContainer'));
