3333d0ece6520353469133b2102469d4d9ccbde2
František Hába <frantisek@apiary.io>
2016-03-02 18:55:29 +0100

    Support primitive types rendering.

---
+++ src/Error/Error.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import merge from 'lodash/merge';
+
+class Error extends React.Component {
+  static propTypes = {
+    errorMessage: React.PropTypes.string,
+    style: React.PropTypes.object,
+  };
+
+  get style() {
+    const style = {
+      base: {
+        width: '100%',
+        height: 'auto',
+        color: '#4C5264',
+        backgroundColor: '#fff',
+        border: '1px solid rgb(213, 222, 237)',
+        paddingTop: '10px',
+        paddingBottom: '14px',
+        paddingLeft: '16px',
+        paddingRight: '16px',
+        borderRadius: '3px',
+        boxShadow: 'rgb(240, 241, 244) 0px 1px 1px 0px',
+      },
+      title: {
+        fontFamily: 'Source Sans Pro',
+        fontSize: '18px',
+        lineHeight: '150%',
+        fontWeight: '600',
+        marginBottom: '2px',
+        color: '#DD1711',
+      },
+      text: {
+        fontFamily: 'Source Sans Pro',
+        fontSize: '16px',
+        lineHeight: '125%',
+      },
+    };
+
+    return merge(style, this.props.style || {});
+  }
+
+  render() {
+    return (
+      <div style={this.style.base}>
+        <h3 style={this.style.title}>Uh-oh, an error occurred!</h3>
+        <p style={this.style.text}>{this.props.errorMessage}</p>
+      </div>
+    );
+  }
+}
+
+export default Error;
---
+++ src/Primitive/Primitive.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import merge from 'lodash/merge';
+
+import Row from '../Row/Row';
+import Type from '../Type/Type';
+import Column from '../Column/Column';
+import Value from '../Value/Value';
+import Description from '../Description/Description';
+
+
+class Primitive extends React.Component {
+  static propTypes = {
+    element: React.PropTypes.object,
+    style: React.PropTypes.object,
+  };
+
+  static contextTypes = {
+    theme: React.PropTypes.object,
+  };
+
+  get style() {
+    const { BORDER_COLOR } = this.context.theme;
+
+    const style = {
+      base: {
+
+      },
+      typeHeader: {
+        borderBottom: `1px solid ${BORDER_COLOR}`,
+        paddingBottom: '8px',
+        paddingLeft: '0px',
+        paddingTop: '4px',
+      },
+      type: {
+        root: {
+          fontSize: '12px',
+        },
+      },
+    };
+
+    return merge(style, this.props.style || {});
+  };
+
+  get type() {
+    const element = this.props.element;
+
+    if (element.meta && element.meta.id) {
+      return element.meta.id;
+    }
+
+    return element.element;
+  };
+
+  render() {
+    return (
+      <Column style={this.style.base}>
+        <Row style={this.style.typeHeader}>
+          <Type
+            type={this.type}
+            style={this.style.type}
+          />
+        </Row>
+
+        <Row>
+          <Description element={this.props.element} />
+        </Row>
+
+        <Row>
+          <Value element={this.props.element} />
+        </Row>
+      </Column>
+    );
+  };
+}
+
+export default Primitive;
