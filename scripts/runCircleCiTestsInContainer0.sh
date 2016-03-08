bf1e6e92afa2fef3cb231842657ff529bfae95c0
František Hába <frantisek@apiary.io>
2016-03-08 15:11:07 +0100

    Update & test latest version of Node.js 0.12.x.

--- scripts/runCircleCiTestsInContainer0.sh
+++ scripts/runCircleCiTestsInContainer0.sh
@@ -4,5 +4,5 @@
 . ~/nvm/nvm.sh
 
 # Run tests
-nvm use 0.12 && npm -g install npm@latest && rm -rf node_modules && npm install && npm test
 nvm use 0.10.43 && npm -g install npm@latest && rm -rf node_modules && npm install && npm test
+nvm use 0.12.11 && npm -g install npm@latest && rm -rf node_modules && npm install && npm test
