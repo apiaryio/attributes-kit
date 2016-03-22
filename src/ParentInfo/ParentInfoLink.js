78d694df1ec2ad212e02cce0e912ba074aa02a6e
František Hába <frantisek@apiary.io>
2016-03-22 20:50:37 +0100

    Redesign ‘ParentInfoLink’ component.

--- src/ParentInfo/ParentInfoLink.js
+++ src/ParentInfo/ParentInfoLink.js
@@ -2,11 +2,14 @@ import React from 'react';
 import radium from 'radium';
 import merge from 'lodash/merge';
 
+import {
+  isInherited,
+  isIncluded,
+} from '../elements/element';
+
 class ParentInfoLink extends React.Component {
   static propTypes = {
     element: React.PropTypes.object,
-    show: React.PropTypes.bool,
-    showBullet: React.PropTypes.bool,
   };
 
   static contextTypes = {
@@ -19,19 +22,17 @@ class ParentInfoLink extends React.Component {
 
     const style = {
       base: {
-        cursor: 'pointer',
-        color: DESCRIPTION_COLOR,
         fontFamily: 'Source Sans Pro',
-        fontWeight: '600',
-        fontSize: '14px',
-        color: '#4C5264',
-        borderBottom: '1px solid #EAEBEE',
+        fontStyle: 'italic',
+        fontSize: '12px',
+        color: '#8A93A3',
       },
-      bullet: {
+      link: {
         cursor: 'pointer',
-        color: DESCRIPTION_COLOR,
-        fontSize: '90%',
-        textDecoration: 'none',
+
+        ':hover': {
+          textDecoration: 'underline',
+        },
       },
     };
 
@@ -42,7 +43,7 @@ class ParentInfoLink extends React.Component {
     return this.props.element.meta && this.props.element.meta.ref;
   }
 
-  handleClick(event) {
+  handleClick = (event) => {
     if (this.context.onElementLinkClick) {
       return this.context.onElementLinkClick(
         this.props.element.meta.ref, this.props.element, event);
@@ -50,19 +51,27 @@ class ParentInfoLink extends React.Component {
   }
 
   render() {
-    if (this.props.show === false) {
-      return null;
+    let text;
+
+    if (isInherited(this.props.element)) {
+      text = 'Inherited from';
+    }
+
+    if (isIncluded(this.props.element)) {
+      text = 'Included from';
     }
 
     return (
-      <a onClick={this.handleClick.bind(this)}>
-        {
-          this.props.showBullet === true && this.ref &&
-          <span style={this.style.bullet}>▸&nbsp;</span>
-        }
-
-        <span style={this.style.base}>{this.ref}</span>
-      </a>
+      <span style={this.style.base}>
+        {text}
+        &nbsp;
+        <a
+          onClick={this.handleClick}
+          style={this.style.link}
+        >
+          {this.ref}
+        </a>
+      </span>
     );
   };
 }
