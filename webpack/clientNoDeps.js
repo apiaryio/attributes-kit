2234871a420313d495df4afeaa6c653157d8dbea
František Hába <frantisek@apiary.io>
2016-04-13 02:10:38 +0200

    Fix ‘clientNoDeps’ build, list all dev dependencies as externals.

--- webpack/clientNoDeps.js
+++ webpack/clientNoDeps.js
@@ -1,44 +1,45 @@
+import lodash from 'lodash';
 import merge from 'lodash/merge';
+import keys from 'lodash/keys';
+import each from 'lodash/each';
 
 import clientWebpackConfig from './client';
+
+const packageJson = require('../package.json');
+
+let externals = {
+  'abagnale/lib/abagnale': {
+    root: 'abagnale',
+    commonjs2: 'abagnale',
+    commonjs: 'abagnale',
+    amd: 'abagnale',
+  },
+};
+
+each(keys(lodash), => {
+  externals[`lodash/${method}`] = {
+    root: `lodash/${method}`,
+    commonjs2: `lodash/${method}`,
+    commonjs: `lodash/${method}`,
+    amd: `lodash/${method}`,
+  };
+});
+
+each(keys(packageJson.devDependencies), (devDependency) => {
+  externals[devDependency] = {
+    root: devDependency,
+    commonjs2: devDependency,
+    commonjs: devDependency,
+    amd: devDependency,
+  };
+});
 
 export default merge({}, clientWebpackConfig, {
   output: {
     filename: 'attributes-kit-no-deps.js',
   },
 
-  externals: {
-    'react': {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
-    },
-    'react-dom': {
-      root: 'react-dom',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
-    },
-    'marked': {
-      root: 'marked',
-      commonjs2: 'marked',
-      commonjs: 'marked',
-      amd: 'marked',
-    },
-    'lodash': {
-      root: 'lodash',
-      commonjs2: 'lodash',
-      commonjs: 'lodash',
-      amd: 'lodash',
-    },
-    'eidolon': {
-      root: 'eidolon',
-      commonjs2: 'eidolon',
-      commonjs: 'eidolon',
-      amd: 'eidolon',
-    },
-  },
+  externals,
 
   devtool: 'source-map',
 });
