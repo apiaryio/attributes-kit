183b69441039aefdc106094e33d1f56e7dc4761c
František Hába <frantisek@apiary.io>
2016-05-23 16:53:43 +0200

    Move the components map to the ‘Resources’ folder.

--- src/Resources/refractToComponentMap.js
+++ src/Resources/refractToComponentMap.js
@@ -1,7 +1,7 @@
-import PrimitiveComponent from './Components/Primitive/Primitive';
-import ArrayComponent from './Components/Array/Array';
-import SelectComponent from './Components/Select/Select';
-import ObjectComponent from './Components/Object/Object';
+import PrimitiveComponent from '../Components/Primitive/Primitive';
+import ArrayComponent from '../Components/Array/Array';
+import SelectComponent from '../Components/Select/Select';
+import ObjectComponent from '../Components/Object/Object';
 
 export default {
   /**
