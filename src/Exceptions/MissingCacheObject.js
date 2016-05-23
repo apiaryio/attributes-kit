d8082cc8ce482e8e0c37ac9af6fe678b814cd399
František Hába <frantisek@apiary.io>
2016-05-23 16:56:23 +0200

    ‘MissingCacheObjectException’ inherits from the ‘Error’ class.

--- src/Exceptions/MissingCacheObject.js
+++ src/Exceptions/MissingCacheObject.js
@@ -1,13 +1,15 @@
+import dedent from 'dedent';
+
 function MissingCacheObjectException(element) {
   this.name = 'MissingCacheObjectException';
 
-  this.message = `
-    The element is missing the ‘cache’ object. Please check that the the Refract
-    tree has been preprocessed.
-
-    Element is ‘${JSON.stringify(element)}’.
+  this.message = dedent`
+    The element is missing the ‘cache’ object. Please check that the the Refract \
+    tree has been preprocessed. Element is ‘${JSON.stringify(element, null, 2)}’.
   `;
 }
+
+MissingCacheObjectException.prototype = new Error;
 
 export {
   MissingCacheObjectException,
