a6260546a4b966bc520cdabef50cb3059eaa5ec5
František Hába <frantisek@apiary.io>
2016-06-02 14:51:43 +0200

    Render type ‘object’ for objects in an enum.

--- src/Components/ObjectHeader/ObjectHeader.js
+++ src/Components/ObjectHeader/ObjectHeader.js
@@ -11,6 +11,7 @@ import SampleToggle from '../SampleToggle/SampleToggle';
 import {
   hasSamples,
   isArray,
+  isEnum,
 } from '../../Modules/ElementUtils/ElementUtils';
 
 @Radium
@@ -98,7 +99,7 @@ class ObjectHeader extends React.Component {
 
         <Column>
           {
-            isArray(this.props.parentElement) &&
+            (isArray(this.props.parentElement) || isEnum(this.props.parentElement)) &&
               <Type
                 type="object"
                 onClick={this.props.expandableCollapsible && this.props.onTypeClick}
