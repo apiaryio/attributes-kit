f96fd6b6d45b20135013e95f1e3386925e844ef6
František Hába <frantisek@apiary.io>
2016-06-07 10:43:02 +0200

    Fix the ‘Cannot read property 'content' of undefined’ error.

--- playground/parseMson.js
+++ playground/parseMson.js
@@ -2,14 +2,21 @@ import protagonist from 'protagonist';
 
 export default function parseMson(mson, cb) {
   protagonist.parse(mson.trim(), (err, parseResult) => {
-    let dataStructureElements;
+    let dataStructureElements = [];
 
     if (err) {
       return cb(err);
     }
 
-
-    dataStructureElements = parseResult.content[0].content[0].content;
+    if (
+      parseResult.content &&
+      parseResult.content[0] &&
+      parseResult.content[0].content &&
+      parseResult.content[0].content[0] &&
+      parseResult.content[0].content[0].content
+    ) {
+      dataStructureElements = parseResult.content[0].content[0].content;
+    }
 
     dataStructureElements = dataStructureElements.map((element) =>
       element.content[0]
