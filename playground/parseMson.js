85ae3b48cb593194e8aeb74a33222444079f75e9
Jakub Mikulas <jakub@mikul.as>
2017-03-31 15:51:53 +0200

    refactor(playground): mson parser

--- playground/parseMson.js
+++ playground/parseMson.js
@@ -1,27 +1,8 @@
 import get from 'lodash/get';
-import isArray from 'lodash/isArray';
 import drafterjs from 'drafter.js';
 
 export default function parseMson(mson, cb) {
-  drafterjs.parse(mson.trim(), {}, (err, parseResult) => {
-    let dataStructureElements = [];
-
-    if (err) {
-      return cb(err);
-    }
-
-    if (get(parseResult, 'content[0].content[0].content')) {
-      dataStructureElements = parseResult.content[0].content[0].content;
-    }
-
-    if (!isArray(dataStructureElements)) {
-      return cb(null, []);
-    }
-
-    dataStructureElements = dataStructureElements.map((element) =>
-      element.content[0]
-    );
-
-    return cb(null, dataStructureElements);
-  });
+  drafterjs.parse(mson.trim(), {}, (err, parseResult = {}) =>
+    cb(err, get(parseResult, 'content[0].content[0].content', []).map(el => el.content[0]))
+  );
 }
