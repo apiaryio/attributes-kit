8ce043a667b3e5f260a5403b35c324c8c2b5afc4
Jakub Mikulas <jakub@mikul.as>
2017-04-22 00:45:36 +0200

    fix(Attributes): allow meta.id to be an object

--- src/Components/Attributes/Attributes.js
+++ src/Components/Attributes/Attributes.js
@@ -110,11 +110,15 @@ class Attributes extends React.Component {
     // `dataStructure.meta.id` is name of the data structure.
     const dataStructuresIndex = props.dataStructuresIndex || (
       reduce(dataStructures, (result, dataStructure) => {
-        result[dataStructure.meta.id] = dataStructure;
+        if (typeof dataStructure.meta.id === 'object') {
+          result[dataStructure.meta.id.content] = dataStructure;
+        } else {
+          result[dataStructure.meta.id] = dataStructure;
+        }
+
         return result;
       }, {})
     );
-
 
     const dereferencedDataStructures = props.dereferencedDataStructures || (
       map(dataStructures, (dataStructure) =>
