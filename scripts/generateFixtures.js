d63aa902db93a9f06bc3013feabc0826e4252929
Jakub Mikulas <jakub@mikul.as>
2017-04-24 16:06:00 +0200

    refactor: remove async package dependency
    Use Promises instead

--- scripts/generateFixtures.js
+++ scripts/generateFixtures.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import async from 'async';
 import ReactDomServer from 'react-dom/server';
 import msonZoo from 'mson-zoo';
 import jsBeautify from 'js-beautify';
 import fs from 'fs';
 import path from 'path';
-import drafter from 'drafter.js';
-import get from 'lodash/get';
+import parse from '../lib/parse';
 
 import AttributesKit from '../dist/attributes-kit';
 
@@ -16,14 +14,12 @@ if (!fs.existsSync(fixtureLocation)) {
   fs.mkdirSync(fixtureLocation);
 }
 
-async.forEachOfLimit(msonZoo.samples, 10, (sample, sampleIndex, next) => {
-  const header = '# Data Structures';
-
-  const dataStructureElements = get(drafter.parse(`${header}\n${sample.fileContent}`, {}), 'content[0].content[0].content', []).map(el => el.content[0]);
+msonZoo.samples.forEach((sample) => {
+  const dataStructureElements = parse(sample.fileContent);
   if (!dataStructureElements || dataStructureElements.length === 0) {
     console.error('An error occured during, no data structure elements were returned.');
     console.error(`Name of the file is ‘${sample.fileName}.’`);
-    return next(new Error('No data structure elements were returned.'));
+    throw new Error('No data structure elements were returned.');
   }
 
   const renderedElement = React.createElement(AttributesKit.Attributes, {
@@ -36,12 +32,6 @@ async.forEachOfLimit(msonZoo.samples, 10, (sample, sampleIndex, next) => {
   const htmlString = jsBeautify.html(ReactDomServer.renderToStaticMarkup(renderedElement));
 
   fs.writeFileSync(path.join(fixtureLocation, sample.fileName), htmlString);
-
-  next();
-}, (err) => {
-  if (err) {
-    process.exit(1);
-  }
-
-  console.log('All good!');
 });
+
+console.log('All good!');
--- scripts/runClientBuild.js
+++ scripts/runClientBuild.js
@@ -1,5 +1,4 @@
 import 'colors';
-import async from 'async';
 import Table from 'cli-table';
 import webpack from 'webpack';
 
@@ -12,75 +11,66 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'production';
 }
 
-function createClientBuild(webpackConfig, cb) {
-  var table = new Table();
+function createClientBuild(webpackConfig) {
+  const table = new Table();
 
   table.push(
     [
       'File Name'.grey.bold,
-      (webpackConfig.output.filename).white
+      (webpackConfig.output.filename).white,
     ],
     [
       'Environment'.grey.bold,
-      (process.env.NODE_ENV).white
+      (process.env.NODE_ENV).white,
     ],
     [
       'Externals'.grey.bold,
-      (Object.keys(webpackConfig.externals || {}).join('\n')).white
+      (Object.keys(webpackConfig.externals || {}).join('\n')).white,
     ],
     [
       'Library Target'.grey.bold,
-      (webpackConfig.output.libraryTarget).white
+      (webpackConfig.output.libraryTarget).white,
     ],
     [
       'Dev Tools'.grey.bold,
-      (webpackConfig.devtool || '').white
+      (webpackConfig.devtool || '').white,
     ],
   );
 
   console.log();
   console.log(table.toString());
 
-  const compiler = webpack(webpackConfig, (err, stat) => {
-    if (err) {
-      console.error('\nAn error occured, unable to create a server build.'.red.bold);
-      console.error(err);
-      cb(err);
-    }
-    console.log(
-      '\nSuccess!'.green.bold,
-      `(${((stat.endTime - stat.startTime) / 1000)} seconds)\n`.grey
-    );
+  return new Promise((resolve, reject) => {
+    webpack(webpackConfig, (err, stat) => {
+      if (err || stat.errors) {
+        console.error('\nAn error occured, unable to build.'.red.bold);
+        console.error(err, stat.errors);
+        return reject(err);
+      }
 
+      console.log(
+        '\nSuccess!'.green.bold,
+        `(${((stat.endTime - stat.startTime) / 1000)} seconds)\n`.grey
+      );
 
-    Object.keys(stat.compilation.assets).forEach(function(asset) {
-      console.log('+'.grey, (asset).white);
-    })
+      Object.keys(stat.compilation.assets).forEach((asset) => {
+        console.log('+'.grey, (asset).white);
+      });
 
-    cb();
+      return resolve();
+    });
   });
 };
 
 console.log('Creating the client builds…'.white.bold);
 
-async.series([
-  function(next) {
-    createClientBuild(webpackClientFullConfig, next);
-  },
-  function(next) {
-    createClientBuild(webpackClientFullMinConfig, next);
-  },
-  function(next) {
-    createClientBuild(webpackClientNoReactConfig, next);
-  },
-  function(next) {
-    createClientBuild(webpackClientNoReactMinConfig, next);
-  },
-], function(error) {
-  if (error) {
-    console.error('\nAn error occured, unable to create a server build.'.red.bold);
-    throw error;
-  }
-
-  console.log('\nAll good!\n'.green.bold);
-});
+Promise.resolve()
+  .then(() => createClientBuild(webpackClientFullConfig))
+  .then(() => createClientBuild(webpackClientFullMinConfig))
+  .then(() => createClientBuild(webpackClientNoReactConfig))
+  .then(() => createClientBuild(webpackClientNoReactMinConfig))
+  .then(() => { console.log('\nAll good!\n'.green.bold); })
+  .catch((error) => {
+    console.error('\nAn error occured, unable to build.'.red.bold, error);
+    process.exit(1);
+  });
