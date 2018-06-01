71ec18bdfabcdca1e962b0c426ea4da84a497663
Standa Opichal <opichals@gmail.com>
2018-06-01 17:16:09 +0200

    test: fix comparing element style attribute value
    The order of the rules in the style attribute value changes so it
    is now sorted before doing the comparison.
    
    The change unveils the fact the radium v0.24.0 has the follwing prefixer
    issue fixed.
    
    From the test output:
    ```
    - actual, + expected
    
      -display:-moz-box
      -display:-ms-flexbox
      -display:-webkit-box
      -display:-webkit-flex
      -display:flex
      +display:-webkit-box,-moz-box,-ms-flexbox,-webkit-flex,flex
    ```

--- test/fixturesComparision.js
+++ test/fixturesComparision.js
@@ -5,24 +5,47 @@ import jsBeautify from 'js-beautify';
 import fs from 'fs';
 import path from 'path';
 import assert from 'assert';
-import dedent from 'dedent';
+import minim from 'minim';
 
 import parseMson from '../playground/parseMson';
 import { Attributes } from '../dist/attributes-kit-server';
 
 describe('Comparision with reference fixtures', () => {
 
+  /**
+   *  Sort the style attribute rules and formats them for easy comparison
+   *
+   *  converts this:
+   *     `<div style="width:100%;height:auto;display:flex;flex-direction:row;">`
+   *
+   *  into:
+   *  `<div style="
+   *   display:flex
+   *   flex - direction: row
+   *   height:auto
+   *   width:100%
+   *   ">`
+   */
+  const formatStyle = html => html.replace(
+    /style\=\"([^\"]+)\"/g,
+    (m, s) => `style="\n${s.split(';').sort().filter(s => s.length).join('\n')}\n"`);
+
+
   msonZoo.samples.forEach((sample) => {
     let renderedElement = null;
     let htmlString = null;
 
     describe(`If I render ${sample.fileName} on the server`, (done) => {
-      let header = '# Data Structures';
+      const header = '# Data Structures';
 
       parseMson(`${header}\n${sample.fileContent}`, (err, dataStructureElements) => {
         if (err) {
           return done(err);
         }
+
+        // to minim
+        const minimNamespace = minim.namespace();
+        dataStructureElements = dataStructureElements.map(d => minimNamespace.fromRefract(d));
 
         renderedElement = React.createElement(Attributes, {
           element: dataStructureElements[0],
@@ -43,7 +66,7 @@ describe('Comparision with reference fixtures', () => {
         );
 
         it('They should be equal', () => {
-          assert.equal(htmlString, reference);
+          assert.equal(formatStyle(htmlString), formatStyle(reference));
         });
       });
     });
