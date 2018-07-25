550114f2cb8770173ce70f8230feb0de054b1f4c
Standa Opichal <opichals@gmail.com>
2018-07-25 11:49:09 +0200

    feat: minify production js
    This makes sure we are not distributing any examples in the
    jsdoc documentation which is licenced under CC0 in the case of
    the `lodash` libs.

--- webpack/playground.js
+++ webpack/playground.js
@@ -2,6 +2,7 @@ import merge from 'lodash/merge';
 import path from 'path';
 import webpack from 'webpack';
 import baseConfig from './base';
+import UglifyJsPlugin from 'webpack/lib/optimize/UglifyJsPlugin';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -29,6 +30,8 @@ export default merge({}, baseConfig, {
     let pluginsArray = [];
     if (!isProduction) {
       pluginsArray.push(new webpack.HotModuleReplacementPlugin());
+    } else {
+      pluginsArray.push(new UglifyJsPlugin());
     }
 
     return pluginsArray;
